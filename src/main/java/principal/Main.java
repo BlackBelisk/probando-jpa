4b93c13fae6b96d474850d4d6a2362ca4c299d3c
franco.biardo@fing.edu.uy <tryden.EN.ES@gmail.com>
2024-11-17 00:35:35 -0300

    Cardinalidades

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -3,7 +3,13 @@ package principal;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Usuarios: \n -> Ninguno o un cliente, Ninguna o una aerolinea;");
+		System.out.println("Aerolineas: \n -> Un usuario, Una o muchas rutas finalizadas;");
+		System.out.println("Clientes: \n -> Un usuario, Una o muchas reservas");
+		System.out.println("Reservas: \n -> Un cliente, Un vuelo");
+		System.out.println("Vuelos: \n -> Ninguna o muchas reservas, Una ruta finalizada");
+		System.out.println("Rutas Finalizadas: \n -> Ninguno o muchos vuelos, Una aerolinea");
+		
 	}
 
 }
